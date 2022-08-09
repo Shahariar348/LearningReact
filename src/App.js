d92d12bf9fd08b3b68c45afec20aabb7d893f2b9
Shahariar alom <mdshahariar348@gmail.com>
2022-08-09 10:13:48 +0600

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Render my app Successfully in react 2022</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
